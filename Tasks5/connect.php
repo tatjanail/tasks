e8b78fa047f6651d3aefd0a6614d9dd82859d072
user <user@kab305-9>
2019-03-20 20:02:13 +0200

    20.03.2019

---
+++ Tasks5/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$pdo = PDO('mysql:host=localhost;dbname=test2000;charset=utf8',
+        'root',
+        '',
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+session_start();
+
+
+
